32b8771bff2e184dd834668f8a46f6da4d7c619e
nick <nik.bur2009@yandex.ru>
2020-10-06 14:18:39 +0300

    initial 3

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ juliet_res_dir = juliet_res_dir
 svace_res_dir = svace_res_dir
 cwe_num_list = cwe_num_list
 
-#svace_info = mine_info("svace", svace_res_dir, cwe_num_list, svace_warning_list)
-#juliet_info = mine_info("juliet", juliet_res_dir, cwe_num_list, cwe_num_list)
+svace_info = mine_info("svace", svace_res_dir, cwe_num_list, svace_warning_list)
+juliet_info = mine_info("juliet", juliet_res_dir, cwe_num_list, cwe_num_list)
 
-#save_info(svace_info, svace_data_path)
-#save_info(juliet_info, juliet_data_path)
+save_info(svace_info, svace_data_path)
+save_info(juliet_info, juliet_data_path)
 
 svace_info = load_info(svace_data_path)
 juliet_info = load_info(juliet_data_path)
-8397
-6491
 
 comparison = compare_analyzers_info(copy.deepcopy(svace_info), copy.deepcopy(juliet_info), "lines",
                                   eur_params=lines_params)
--- projectLib/projectConfig.py
+++ projectLib/projectConfig.py
@@ -1,14 +1,12 @@
-juliet_res_dir = "/home/gorchichka/juliet/manifest.xml"
-svace_res_dir = "/home/gorchichka/svc/C.svres"
+
+juliet_res_dir = "/home/nick/C/manifest.xml"
+svace_res_dir = "/home/nick/C.svres"
 cwe_num_list = ["CWE121_Stack_Based_Buffer_Overflow",
                 "CWE122_Heap_Based_Buffer_Overflow",
                 "CWE134_Uncontrolled_Format_String" ]
-juliet_data_path = "/home/gorchichka/svc/save_info/juliet_info.data"
-svace_data_path = "/home/gorchichka/svc/save_info/svc_info.data"
-save_comp_results_dir = "/home/gorchichka/svc/save_results"
-
+juliet_data_path = "/home/nick/svace/save_info/juliet_info.data"
+svace_data_path = "/home/nick/svace/save_info/svc_info.data"
+save_comp_results_dir = "/home/nick/svace/save_results"
 svace_warning_list = None
-#svace_warning_list = ['MEMORY_LEAK.EX',
- #                     'DEREF_OF_NULL.EX.COND']
 
-lines_params = {"distance": 1}
+lines_params = {"distance": 0}
